d6266249b443f5e1d9a4061c998b33fd6d662853
ToriLutz <vglutz@comcast.net>
2025-05-08 14:30:19 -0700

    made thing #2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -7,32 +7,72 @@ class Program
         Console.WriteLine("Please insert your grade: ");
         string grade = Console.ReadLine();
         int number = int.Parse(grade);
+        string letterGrade = "";
+        string digit = (number / 10).ToString();
+        int digitGrade = int.Parse(digit);
+        string final= "";
+
+    
+
+        if (number >= 70)
+        {   
+            Console.WriteLine("You have passed!");
+        }
+        else if(number < 70)
+        {
+            
+            Console.WriteLine("You Shall Not Pass");
+        }
 
         if (number > 90 || number == 90)
         {   
-            Console.WriteLine("Your grade is an A");
-            Console.WriteLine("You have passed!");
+             letterGrade = "A";
+            
         }
         else if (number > 80 || number == 80)
         {
-            Console.WriteLine("Your grade is a B");
-            Console.WriteLine("You have passed!");
+             letterGrade = "B";
+           
         }
         else if (number > 70 || number == 70)
         {
-            Console.WriteLine("Your grade is a C");
-            Console.WriteLine("You have passed!");
+    
+            letterGrade = "C";
         }
         else if (number > 60 || number == 60)
         {
-            Console.WriteLine("Your grade is a D");
-            Console.WriteLine("You Shall Not Pass");
+             letterGrade = "D";
+            
         }
         else if(number < 60)
         {
-            Console.WriteLine("F!");
-            Console.WriteLine("You Shall Not Pass");
+           letterGrade = "F";
+          
+        }
+        
+        if (letterGrade != "F")
+        {
+            if (digitGrade <= 3)
+            {
+                final = "-";
+            }
+            else if (digitGrade >= 7)
+            {
+                final = "+";
+            }
+            else
+            {
+                final = "";
+            }
+        }
+        else
+        {
+            final = "";
         }
 
+        Console.WriteLine($"your grade is {final} {letterGrade}");
+
     }
+
+
 }

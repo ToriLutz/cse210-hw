e3aabca4145a941cb0fabfd76d7117d45e460bf2
ToriLutz <vglutz@comcast.net>
2025-05-08 09:35:44 -0700

    first  2 core requirements

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Please insert your grade: ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+
+        if (number > 90 || number == 90)
+        {   
+            Console.WriteLine("Your grade is an A");
+            Console.WriteLine("You have passed!");
+        }
+        else if (number > 80 || number == 80)
+        {
+            Console.WriteLine("Your grade is a B");
+            Console.WriteLine("You have passed!");
+        }
+        else if (number > 70 || number == 70)
+        {
+            Console.WriteLine("Your grade is a C");
+            Console.WriteLine("You have passed!");
+        }
+        else if (number > 60 || number == 60)
+        {
+            Console.WriteLine("Your grade is a D");
+            Console.WriteLine("You Shall Not Pass");
+        }
+        else if(number < 60)
+        {
+            Console.WriteLine("F!");
+            Console.WriteLine("You Shall Not Pass");
+        }
+
     }
 }

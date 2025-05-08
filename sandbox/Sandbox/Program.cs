2a759d13f80431af19cf97bb09e23a448a60be30
ToriLutz <vglutz@comcast.net>
2025-05-08 09:07:03 -0700

    lesson one learning

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("This is in c#");
+        Console.WriteLine("What is your favorite color?: ");
+
+        string color = Console.ReadLine();
+        Console.WriteLine(color);
+
+
+
     }
 }

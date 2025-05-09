1e58340c2d9abb75c8aeb148bc1b7e1c316d1293
ToriLutz <vglutz@comcast.net>
2025-05-08 20:01:08 -0700

    core requirements

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int inputNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(inputNumber);
+        DisplayResult(userName, squaredNumber);
+
     }
-}
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        int favNum = int.Parse(Console.ReadLine());
+        return favNum;
+    }
+
+    static int SquareNumber(int number){
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square){
+         Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+    }

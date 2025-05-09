b4fa6abcb4ec3ac9c6c1efa41c72d8d6b426de82
ToriLutz <vglutz@comcast.net>
2025-05-08 18:44:43 -0700

    did the core challenges

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+       Random randomGenerator = new Random(); 
+       int number = randomGenerator.Next(1, 100);
+       Console.WriteLine($"The Magic number is {number}"); 
+       string answer = "0";
+       int magicNumber = int.Parse(answer);
+
+
+       do{
+        Console.WriteLine("Pick a number: ");
+        answer = Console.ReadLine(); 
+        magicNumber = int.Parse(answer); 
+        
+            if (magicNumber == number)
+            {
+                Console.WriteLine("Congratulations you did it!");
+            }
+            else if (magicNumber > number)
+            {
+                Console.WriteLine("lower");
+            }
+            else if (magicNumber < number)
+            {
+                Console.WriteLine("Higher");
+            }
+
+       }
+       while (magicNumber != number);
     }
 }

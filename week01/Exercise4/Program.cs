ad06229ccbc00ccbd5198af67df2a931b92de015
ToriLutz <vglutz@comcast.net>
2025-05-08 19:36:27 -0700

    Core Requirements

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers; 
+        numbers = new List<int>();
+        string number = "1";
+        int listAdd = int.Parse(number);
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do{
+            Console.WriteLine("Enter a number:");
+            number = Console.ReadLine();
+            listAdd = int.Parse(number);
+                if (listAdd != 0)
+                {
+                     numbers.Add(listAdd);
+                }
+        }   
+        while (listAdd != 0);
+        int sum = numbers.Sum();
+        double avg = numbers.Average();
+        int max = numbers.Max();
+
+        Console.WriteLine("Your list of numbers: " + string.Join(", ", numbers));
+        Console.WriteLine($"Your sum is: {sum}");
+        Console.WriteLine($"Your Average is: {avg}");
+        Console.WriteLine($"The largest number is: {max}");
+
+    
     }
 }

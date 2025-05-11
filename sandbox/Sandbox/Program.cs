0f879a7ae44e379c17b7e230c989fa7614d76da6
ToriLutz <vglutz@comcast.net>
2025-05-10 20:36:15 -0700

    resume assignment added classes and objects

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,14 +5,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-        Console.WriteLine("This is in c#");
-        Console.WriteLine("What is your favorite color?: ");
+       Job job1 = new Job();
 
-        string color = Console.ReadLine();
-        Console.WriteLine(color);
+        job1.name = "Software Engineer";
+        job1.company = "Microsoft";
+        job1.startYear = 2019;
+        job1.endYear = 2022;
 
+        job1.Resume();
 
 
     }
+}
+ class Job{
+  public string name;
+  public string company;
+  public int startYear; 
+  public int endYear; 
+
+   public void Resume()
+  {
+    Console.WriteLine($"{name} ({company}), {startYear}-{endYear}");
+  }
 }
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+       Job job1 = new Job();
+
+        job1.name = "Software Engineer";
+        job1.company = "Microsoft";
+        job1.startYear = 2019;
+        job1.endYear = 2022;
+
+        job1.Resume();
+
+
     }
+}
+ class Job{
+  public string name;
+  public string company;
+  public int startYear; 
+  public int endYear; 
+
+   public void Resume()
+  {
+    Console.WriteLine($"{name} ({company}), {startYear}-{endYear}");
+  }
 }

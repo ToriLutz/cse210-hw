5f1dc76c4dab8556b3570f53c0038f9f6e18807b
ToriLutz <vglutz@comcast.net>
2025-05-12 04:08:19 -0700

    sandbox is sandboxing

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,17 +5,20 @@ class Program
 {
     static void Main(string[] args)
     {
-       
+       addRecord ("124", "Mercy", "24", "cake.txt");
     }
     public static void addRecord (string ID, string name, string age, string filepath)
     {
         try{
 
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter (filepath = true));
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter (@filepath, true))
+            {
+                file.WriteLine(ID + "," + name + "," + age);
+            }
         }
         catch(Exception ex)
         {
-            throw new ApplicationException ("This program did an oopsie", ex);
+            throw new ApplicationException ("This program made an error", ex);
 
         }
     }

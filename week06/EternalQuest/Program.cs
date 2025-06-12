e2a7020104f1331037666d26b9da15e5c851338b
ToriLutz <vglutz@comcast.net>
2025-06-12 09:24:32 -0700

    starting EternalQuest

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,9 +1,68 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+
+    }
+
+    public class Goal
+    {
+        private string _shortName;
+        private string _description;
+        private int _points;
+
+        public void SetName(string ShortName)
+        {
+            _shortName = ShortName;
+        }
+        public string GetName()
+        {
+            return _shortName;
+        }
+        public void SetDesc(string Description)
+        {
+            _description = Description;
+        }
+        public string GetDesc()
+        {
+            return _description;
+        }
+
+        public void SetPoints(int Points)
+        {
+            _points = Points;
+        }
+
+        public int GetPoints()
+        {
+            return _points;
+        }
+
+        public string goal()
+        {
+            return $"{_shortName}, {_description} this activity earns you {_points}";
+        }
+        public void recordEvent()
+        {
+
+        }
+
+        public bool IsCompleted()
+        {
+
+        }
+
+        public string GetDetailsString()
+        {
+
+        }
+
+        public string GetStringRepresentation()
+        {
+            
+        }
     }
 }
